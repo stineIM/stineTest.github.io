c280346296b6d40d41f26af7a719c6922c648579
Stine <93541692+stineIM@users.noreply.github.com>
2024-11-08 12:45:27 +0100

    update

--- js/styling.js
+++ js/styling.js
@@ -2,6 +2,7 @@
 // Funksjoner:
 function setHeading() {
     let fname = prompt("Skriv inn navnet ditt"); 
+    console.log(fname);
     let firstname = document.getElementById("firstname"); 
     let heading = document.getElementById("heading"); 
     heading.innerText = "Velkommen, " + firstname.value; 
