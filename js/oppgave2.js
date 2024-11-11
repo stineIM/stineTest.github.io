47589a648336303349b723ac9bb561ccca2a56fe
Stine <93541692+stineIM@users.noreply.github.com>
2024-11-11 12:49:10 +0100

    added oppgave2

---
+++ js/oppgave2.js
@@ -0,0 +1,14 @@
+/*
+let text = "Velkommen"; 
+let firstname = prompt("Skriv inn navnet ditt: "); 
+console.log(text + ", " + firstname); 
+*/ 
+
+let heading = document.getElementById("header"); 
+heading.style.color = "rgb(34,98,200)";
+
+let container = document.getElementById("container");
+container.innerText = "Dette er sida mi"; 
+container.style.backgroundColor = "rgb(34,98,200)";
+container.style.color = "white"; 
+container.style.fontSize = "40px"; 
