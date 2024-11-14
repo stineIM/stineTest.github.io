4fa3fa596f81ca1b15db5aa5ee0cd5f70b35b4e8
Stine <93541692+stineIM@users.noreply.github.com>
2024-11-14 08:35:04 +0100

    changed styling

--- js/styling.js
+++ js/styling.js
@@ -1,3 +1,7 @@
+function changeColor() {
+    let heading = document.getElementById("heading"); 
+    heading.style.color = "blue"; 
+}
 
 // Funksjoner:
 function setHeading() {
@@ -7,4 +11,12 @@ function setHeading() {
     let heading = document.getElementById("heading"); 
     heading.innerText = "Velkommen, " + firstname.value; 
     heading.style.color = "red"; 
+}
+
+// Ein funksjon som sjekker kva ein har svart på spørsmål
+function checkAnswer() {
+    let q1a = document.getElementById("q1a"); 
+    let q1b = document.getElementById("q1b"); 
+    console.log("Q1a: " + q1a.checked); // Finner ut om radiobutton med id q1a er valt 
+    console.log("Q1b: " + q1b.checked); // Finner ut om radiobutton med id q1b er valt 
 }
